c4870721619693db99b40b2a3cd26dd42a02e257
Pincheira07 <m.pincheira07@ufromail.cl>
2021-06-09 09:34:52 -0400

    Commit2

--- src/main/java/Computador.java
+++ src/main/java/Computador.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class Computador {
+    private String marca;
+    private String modelo;
+    public ArrayList<RAM> memoriasRam = new ArrayList<>();
 }
--- src/main/java/RAM.java
+++ src/main/java/RAM.java
@@ -1,2 +1,52 @@
+import java.util.Scanner;
+
 public class RAM {
+    private String marca;
+    private int capacidad;
+    private int frecuencia;
+
+    public RAM() {
+        registrarMarca();
+        registrarCapacidad();
+        registrarFrecuencia();
+    }
+
+
+    private void registrarFrecuencia() {
+        System.out.println("Ingrese la frecuencia en GHZ: ");
+
+
+
+    }
+
+    private void registrarCapacidad() {
+        System.out.println("Ingrese la capacidad de la RAM: ");
+
+    }
+
+    private void registrarMarca() {
+        String nombre = leerNombre();
+        nombre = this.marca;
+
+    }
+    public static String leerNombre() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese la marca de la RAM");
+        return input.nextLine();
+    }
+
+    public static int leerDatos() {
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+    public static boolean validarFrecuencia(int numero){
+        return 0 <= numero && numero <= 255;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+
 }
+
